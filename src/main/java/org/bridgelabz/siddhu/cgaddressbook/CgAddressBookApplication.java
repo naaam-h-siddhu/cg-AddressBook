b880478d1fee18fb7de929fbfd15deaeaa9f10d0
naaam-h-siddhu <sudhakarsingh2004@gmail.com>
2025-03-01 03:06:31 +0530

    🚀 Initial commit

---
+++ src/main/java/org/bridgelabz/siddhu/cgaddressbook/CgAddressBookApplication.java
@@ -0,0 +1,13 @@
+package org.bridgelabz.siddhu.cgaddressbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgAddressBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CgAddressBookApplication.class, args);
+    }
+
+}
